29e11974f434ad7757a7828c146e009ab5533142
Michael M. Q. Nguyen <michaelng728@gmail.com>
2019-02-23 11:35:34 -0800

    Initial commit.

---
+++ GradWare_OCR/IronOcr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+
+namespace OCR_Prototyping
+{
+    public class IronOcrClass
+    {
+        public IronOcrClass()
+        {
+            var file = "../../test_files/hello.pdf";
+
+            // checks if file exists
+            //if (File.Exists(file))
+            //{
+            //    var fileName = Path.GetFileName(file);
+            //    Console.WriteLine("file exists: {0}", fileName);
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    var currentPath = Path.GetFullPath(file);
+            //    Console.WriteLine("file DNE; looking in: {0}", currentPath);
+            //    Console.ReadLine();
+            //}
+
+            // reads .txt file
+            //string text = File.ReadAllText(file);
+            //Console.WriteLine("Contents of test.txt = {0}", text);
+            //// `{0}` denotes the value of second argument, `text`
+            //Console.ReadLine();
+
+            // reads .pdf file
+            var Ocr = new IronOcr.AutoOcr();
+            var Results = Ocr.ReadPdf(@file);
+            Console.WriteLine(Results.Text);
+            Console.ReadLine();
+        }
+    }
+}
--- GradWare_OCR/Program.cs
+++ GradWare_OCR/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OCR_Prototyping;
 
 namespace GradWare_OCR
 {
@@ -10,6 +11,9 @@ namespace GradWare_OCR
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("SwiftWare");
+            //Console.ReadLine();
+            IronOcrClass ironer = new IronOcrClass();
         }
     }
 }
